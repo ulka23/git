23724294a4412feb9de029f93fcdc78f4a963bad
SZEDER Gábor <szeder@ira.uka.de>
2015-11-22 16:29:25 +0100

    Add tests for git-sh-setup's require_clean_work_tree()
    Add tests that check require_clean_work_tree() in the common cases,
    i.e. on a branch with all combinations of clean and dirty index and
    worktree.
    
    Also add failing tests that demonstrate misbehavior on orphan or
    unborn branches, i.e. that require_clean_work_tree() always exits with
    error on an orphan branch independently from whether the index and
    worktree are clean or dirty, and it does so because of the invalid
    HEAD, meaning that even when it should exit with error, it does so for
    the wrong reason.
    
    Signed-off-by: SZEDER Gábor <szeder@ira.uka.de>

---
+++ t/t2301-require-clean-work-tree.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+test_description='require_clean_work_tree'
+
+. ./test-lib.sh
+
+sh_setup_path="$(git --exec-path)"/git-sh-setup
+
+run_require_clean_work_tree () {
+	(
+		. "$sh_setup_path" &&
+		require_clean_work_tree "do-something"
+	)
+}
+
+test_expect_success 'setup' '
+	test_commit initial file
+'
+
+test_expect_success 'success on clean index and worktree' '
+	run_require_clean_work_tree
+'
+
+test_expect_success 'error on dirty worktree' '
+	echo "Cannot do-something: You have unstaged changes." >expect &&
+	test_when_finished "git reset --hard" &&
+	echo dirty >file &&
+	test_must_fail run_require_clean_work_tree 2>err &&
+	test_cmp expect err
+'
+
+test_expect_success 'error on dirty index' '
+	echo "Cannot do-something: Your index contains uncommitted changes." >expect &&
+	test_when_finished "git reset --hard" &&
+	echo dirty >file &&
+	git add file &&
+	test_must_fail run_require_clean_work_tree 2>err &&
+	test_cmp expect err
+'
+
+test_expect_success 'error on dirty index and worktree' '
+	cat >expect <<-EOF &&
+	Cannot do-something: You have unstaged changes.
+	Additionally, your index contains uncommitted changes.
+	EOF
+	test_when_finished "git reset --hard" &&
+	echo dirty >file &&
+	git add file &&
+	echo dirtier >file &&
+	test_must_fail run_require_clean_work_tree 2>err &&
+	test_cmp expect err
+'
+
+test_expect_failure 'error on clean index and worktree while on orphan branch' '
+	test_when_finished "git checkout master" &&
+	git checkout --orphan orphan &&
+	git reset --hard &&
+	run_require_clean_work_tree
+'
+
+test_expect_failure 'error on dirty index while on orphan branch' '
+	echo "Cannot do-something: Your index contains uncommitted changes." >expect &&
+	test_when_finished "git checkout master" &&
+	git checkout --orphan orphan &&
+	test_must_fail run_require_clean_work_tree 2>err &&
+	test_cmp expect err
+'
+
+test_expect_failure 'error on dirty index and work tree while on orphan branch' '
+	cat >expect <<-EOF &&
+	Cannot do-something: You have unstaged changes.
+	Additionally, your index contains uncommitted changes.
+	EOF
+	test_when_finished "git checkout master" &&
+	git checkout --orphan orphan &&
+	echo dirty >file &&
+	test_must_fail run_require_clean_work_tree 2>err &&
+	test_cmp expect err
+'
+
+test_done
