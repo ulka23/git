5022ebce2d233a90af2fdd00c16873948325733b
SZEDER Gábor <szeder.dev@gmail.com>
2019-01-12 00:28:36 +0100

    t5406-remote-rejects: cd back to trash fixes

--- t/t5406-remote-rejects.sh
+++ t/t5406-remote-rejects.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 test_description='remote push rejects are reported by client'
-test_preserve_cwd=UnfortunatelyYes
 
 . ./test-lib.sh
 
@@ -19,8 +18,14 @@ test_expect_success 'setup' '
 	git commit -a -m 2
 '
 
-test_expect_success 'push reports error' 'test_must_fail git push 2>stderr'
+test_expect_success 'push reports error' '
+	cd child &&
+	test_must_fail git push 2>stderr
+'
 
-test_expect_success 'individual ref reports error' 'grep rejected stderr'
+test_expect_success 'individual ref reports error' '
+	cd child &&
+	grep rejected stderr
+'
 
 test_done
