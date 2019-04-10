01866f4b5f0335d74badc5c1a3d30e5b1684e8e3
SZEDER Gábor <szeder.dev@gmail.com>
2019-04-10 12:17:14 +0200

    travis: let's see whether 'git p4' can deal with Python 3.6

--- ci/run-build-and-tests.sh
+++ ci/run-build-and-tests.sh
@@ -5,6 +5,8 @@
 
 . ${0%/*}/lib.sh
 
+python --version
+
 case "$CI_OS_NAME" in
 windows*) cmd //c mklink //j t\\.prove "$(cygpath -aw "$cache_dir/.prove")";;
 *) ln -s "$cache_dir/.prove" t/.prove;;
